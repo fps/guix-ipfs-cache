b3277a95620d87b9dff95e13c6aae65ff2882a65
Florian Paul Schmidt <mista.tapas@gmx.net>
2018-11-28 15:59:46 +0100

    add initial hacky version of scripts

--- update.sh
+++ update.sh
@@ -1,3 +1,5 @@
+
+echo "#### FINISHED JOBS"
 bash get_build_hashes.sh > current_hashes.txt; 
 for n in `cat current_hashes.txt`; do 
     narinfo="$n.narinfo"
@@ -8,13 +10,23 @@ for n in `cat current_hashes.txt`; do
     fi 
 done
 
+echo "#### GETTING NARINFOS"
 for n in cache/*.narinfo; do 
     cat "$n" | grep ^URL | cut -d / -f 3 > current_nars.txt
 done
 
+
+echo "#### GETTING NARS"
 for n in `cat current_nars.txt`; do
     if [ ! -f "./cache/nar/gzip/$n" ]; then
         echo get: "$n"
         wget https://berlin.guixsd.org/nar/gzip/"$n" -P ./cache/nar/gzip/
     fi
 done
+
+echo "#### UPDATING IPFS"
+ipfs add -r cache > ipfs_log.txt
+
+echo "#### UPDATING IPNS"
+cache_cid=`tail -n 1 ipfs_log.txt | cut -d ' ' -f 2`
+ipfs name publish "$cache_cid"
